90c8ce6f5d6fd1e7a9ba9edea10986d256f83fec
Arif Islam <businessarifislam@gmail.com>
2024-05-12 15:13:22 +0530

    02 best 9 product added

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// let category = document.querySelector(".catagories");
-// let catapop = document.querySelector(".catagory-popup-cnt");
-// let expandmore = document.querySelector(".expand_more");
-// let expandless = document.querySelector("#expand_less");
-// category.onclick = function () {
-//   catapop.classList.toggle("active");
-//   expandless.classList.toggle("active");
-//   expandmore.classList.toggle("active")
-// };
